ca4d502e7a0068d4bcb15adb3ad53fea827c17a5
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2025-12-13 18:26:53 +0100

    Update route.ts

--- src/app/api/paystack/callback/route.ts
+++ src/app/api/paystack/callback/route.ts
@@ -129,7 +129,7 @@ export async function GET(req: NextRequest) {
           if (to) {
             await sendResendMail(
               to,
-              "Your Payment Was Successful 🎉",
+              "Welcome to Curriculum of Light! 💡🎉",
               paymentSuccessTemplate({
                 name: orderData.full_name ?? "Customer",
                 reference: reference as string,

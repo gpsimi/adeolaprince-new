68fb01f2227ce2b9e5076ba37ea91d6768456566
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2026-01-27 10:56:50 +0100

    Update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,20 +7,7 @@ export const metadata = {
 
 export default function ContactPage() {
   return (
-    // <main className="min-h-screen pt-24">
-    //   <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-20">
-    //     <div className="max-w-3xl mx-auto text-center mb-12">
-    // <h1 className="text-4xl md:text-5xl font-serif font-bold">Contact</h1>
-    // <p className="text-muted-foreground mt-4">
-    //   For bookings, speaking engagements or collaborations, send a message below.
-    // </p>
-    //     </div>
-
-    //     <div className="mx-auto container max-w-4xl justify-center items-center">
-    //       <ContactForm />
-    //     </div>
-    //   </div>
-    // </main>
+    
     <div className="min-h-screen pt-20">
       <section className="py-20 md:py-32">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
